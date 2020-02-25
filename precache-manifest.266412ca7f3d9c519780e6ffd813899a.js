9e2bdc72a67bf320bcc852f3c0a46d41e1f4b7f9
prashanthsm8 <37961439+prashanthsm8@users.noreply.github.com>
2020-02-25 23:11:54 +0530

    Updates

---
+++ precache-manifest.266412ca7f3d9c519780e6ffd813899a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17aedba5aa3ecf5b33a63bb94ce4eb58",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "7edbf1f442cdfd5b6311",
+    "url": "/myapp/static/css/main.5002ed6f.chunk.css"
+  },
+  {
+    "revision": "50178188a875b58681d5",
+    "url": "/myapp/static/js/2.e9265ccf.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/myapp/static/js/2.e9265ccf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7edbf1f442cdfd5b6311",
+    "url": "/myapp/static/js/main.9b54c23e.chunk.js"
+  },
+  {
+    "revision": "5e9208f8bdc4936b5d0e",
+    "url": "/myapp/static/js/runtime-main.093b8167.js"
+  }
+]);
